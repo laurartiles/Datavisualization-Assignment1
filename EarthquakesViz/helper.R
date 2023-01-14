0229363a086d237e1db0fc17599488133a4a8182
jFompe <jaime.fompe@gmail.com>
2023-01-14 19:32:45 +0100

    removed commented lines

--- EarthquakesViz/helper.R
+++ EarthquakesViz/helper.R
@@ -1,23 +1,13 @@
 coords2continent = function(points, regionName="continent")
 {  
   countriesSP <- getMap(resolution='low')
-  #countriesSP <- getMap(resolution='high') #you could use high res map from rworldxtra if you were concerned about detail
-  
-  # convert our list of points to a SpatialPoints object
-  
-  # pointsSP = SpatialPoints(points, proj4string=CRS(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
   
   #setting CRS directly to that from rworldmap
   pointsSP = SpatialPoints(points, proj4string=CRS(proj4string(countriesSP)))  
   
-  
   # use 'over' to get indices of the Polygons object containing each point 
   indices = over(pointsSP, countriesSP)
   
-  # return the ADMIN names of each country
-  #indices$ADMIN  
-  #indices$ISO3 # returns the ISO3 code 
-  #indices$continent   # returns the continent (6 continent model)
   if (regionName == 'country') return(fct_explicit_na(indices$ADMIN, "Ocean"))
   # continent
   else return(fct_explicit_na(indices$REGION, "Ocean"))   # returns the continent (7 continent model + Ocean)
